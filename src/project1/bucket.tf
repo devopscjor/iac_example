1d122833087bf7fcf67655348a464725185a09d2
ramiroameneses <122799912+ramiroameneses@users.noreply.github.com>
2024-04-02 15:37:50 -0500

    comment files

--- src/project1/bucket.tf
+++ src/project1/bucket.tf
@@ -1,7 +1,7 @@
-resource "aws_s3_bucket" "bucket_app_example" {
-  bucket = "bucket-metro-${var.env}-bucket-example"
+# resource "aws_s3_bucket" "bucket_app_example" {
+#   bucket = "bucket-metro-${var.env}-bucket-example"
 
-  tags = {
-    Environment = var.env
-  }
-}
+#   tags = {
+#     Environment = var.env
+#   }
+# }
