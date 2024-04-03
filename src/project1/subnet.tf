e6acdc89bafce795e2f8d65eb0204b516e1b8c89
cristian.osorio <devopscjor@gmail.com>
2024-04-03 15:40:15 -0500

    conexion data entre workspace

--- src/project1/subnet.tf
+++ src/project1/subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "main" {
-  vpc_id     = module.vpc.vpc_id
+  vpc_id     = data.terraform_remote_state.workspace_project2.outputs.vpc_id
   cidr_block = "10.0.1.0/24"
 
   tags = {
